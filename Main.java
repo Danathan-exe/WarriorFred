c97663a48635276cb428c6f3b0b5625e58d37484
Daniel <56946882+Danathan-exe@users.noreply.github.com>
2023-11-06 16:43:48 -0800

    Java Files for WarriorFred
    files for game

---
+++ Main.java
@@ -0,0 +1,43 @@
+//Daniel DeBonis
+//Computer Science Java
+//creates 5 warriors and then has them fight then prints out the results
+public class Main {
+
+    public static void main(String[] args) {
+        WarriorFred[] warriors = new WarriorFred[5];
+
+        // Creating and initializing warrior objects
+        for (int i = 0; i < warriors.length; i++) {
+            WarriorFred warrior = new WarriorFred();
+            warrior.setNickName(); // Assign a random nickname
+            warrior.setDamage(); // Assign random damage amount
+            warriors[i] = warrior;
+        }
+        // Print all information with the printInfo method
+        for (WarriorFred w : warriors) {
+            w.printInfo();
+        }
+
+        // Simulating rounds and updating warrior status
+        for (WarriorFred w : warriors) {
+
+            for (int round = 1; round <= 10; round++) {
+                System.out.println("Round " + round + "\n");
+
+                // Simulating damage and reaction for the current warrior in the loop
+                int damageTaken = (round % 5) + 1;
+                w.takeDamage(damageTaken);
+                w.reaction();
+
+                System.out.println();
+            }
+        }
+
+        System.out.println("How did the Warriors fare?\n");
+
+        // Print status for each warrior after all rounds
+        for (WarriorFred warrior : warriors) {
+            warrior.printStatus();
+        }
+    }
+}
---
+++ WarriorFred.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+
+public class WarriorFred {
+
+    private String nickName;
+    private int lifeForce = 10;
+    private int damage;
+//array lists for the class
+    String[] armory = {"Exploding Pumpkin", "Candy Corn", "Paper Bag"};
+    String[] name = {"Fred", "Jerry", "Tom","George", "Berry Allen"};
+    Random rand = new Random();
+
+    public void reSpawn() {
+        lifeForce = 10;
+    }
+
+    //Setters for class WarriorFred
+    public void setDamage(){
+        Random number = new Random();
+        int damageNum = number.nextInt(10) + 1;
+        this.damage = damageNum;
+    }
+    public void setNickName() {
+        int index = rand.nextInt(name.length);
+        this.nickName = name[index];
+    }
+    //getters for the class
+    public int getLifeForce(){
+        return lifeForce;
+    }
+    public int getDamage(){
+        return damage;
+    }
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void takeDamage(int n) {
+        damage += n;
+        if (damage >= 10) {
+            lifeForce--;
+            damage = 0;
+        }
+    }
+
+    public void reaction() {
+        Random r = new Random();
+        String[] sound = {"OW!", "Wham!", "POW!", "Boffo!!", "KaPow!", "That one hurt!"};
+        System.out.println(sound[r.nextInt(sound.length)]);
+    }
+
+    public boolean isDead() {
+        if (lifeForce <= 0) return true;
+        else return false;
+    }
+
+    public boolean isAlive() {
+        if (lifeForce > 0) return true;
+        else return false;
+    }
+
+
+
+    //print Info method
+    public void printInfo() {
+        System.out.println("Nick Name:" + getNickName());
+        System.out.println("Life Force:" + getLifeForce());
+        System.out.println("Damage:" + getDamage());
+        for(int i=0;i<armory.length;i++)
+            System.out.println("Armory:" + this.armory[i]);
+        System.out.println();
+    }
+    public void printStatus(){
+        System.out.println("Warrior " + getNickName() + " " + "Health = " + lifeForce + "  " + "damage = " + getDamage());
+
+    }
+}
+
